ed7f3ae27d174ad8da046a220123726ea9418072
Thibaud Vicat <57406915+Thiabud@users.noreply.github.com>
2021-06-25 10:07:43 +0200

    first test for singularity build

--- bin/candidateLoci.sh
+++ bin/candidateLoci.sh
@@ -15,7 +15,7 @@ SPECIES=$(cat $3| cut -f1)
 treshold1=$(cat $3| cut -f5)
 treshold2=$(cat $3| cut -f6)
 #echo $SPECIES
-SCRIPT='/GeneModelTransfer.git/branches/container/SCRIPT'
+SCRIPT='/GeneModelTransfer.git/branches/container/SCRIPT/'
 #echo $SCRIPT
 function extractSeq {
 	##Extracting each sequence from a fasta in separate files
@@ -46,22 +46,147 @@ function filter_Blastp {
           #------------------------------------------#
 
 mkdir $LRRome/mmseqs
-echo "mmseqs createdb $1 $LRRome/mmseqs/${SPECIES}_genome_db -v 0"
-echo -------------
-echo "mmseqs createdb $LRRome/*_proteins.fasta $LRRome/mmseqs/prot_db  -v 0"
-echo -------------
-echo "mmseqs search $LRRome/mmseqs/prot_db $LRRome/mmseqs/${SPECIES}_genome_db resultDB_aln.m8 tmp -s 8.5 -a -e 0.1 --min-length 10 --merge-query 1 --cov-mode 2 --max-seqs 30000 --sequence-overlap 1000 -v 0"
-echo -------
-echo "mmseqs convertalis $LRRome/mmseqs/prot_db $LRRome/mmseqs/${SPECIES}_genome_db resultDB_aln.m8 $LRRome/mmseqs/res_candidatsLRR_in_$SPECIES.out --format-output query,target,qlen,alnlen,qstart,qend,tstart,tend,nident,pident,gapopen,evalue,bits  -v 0"
-echo ------
-echo "cat $LRRome/mmseqs/res_candidatsLRR_in_$SPECIES.out"
-echo ------
 mmseqs createdb $1 $LRRome/mmseqs/${SPECIES}_genome_db -v 0
 mmseqs createdb $LRRome/*_proteins.fasta $LRRome/mmseqs/prot_db  -v 0
 mmseqs search $LRRome/mmseqs/prot_db $LRRome/mmseqs/${SPECIES}_genome_db resultDB_aln.m8 tmp -s 8.5 -a -e 0.1 --min-length 10 --merge-query 1 --cov-mode 2 --max-seqs 30000 --sequence-overlap 1000 -v 0
 mmseqs convertalis $LRRome/mmseqs/prot_db $LRRome/mmseqs/${SPECIES}_genome_db resultDB_aln.m8 $LRRome/mmseqs/res_candidatsLRR_in_$SPECIES.out --format-output query,target,qlen,alnlen,qstart,qend,tstart,tend,nident,pident,gapopen,evalue,bits  -v 0
 cat $LRRome/mmseqs/res_candidatsLRR_in_$SPECIES.out > res_candidatsLRR_in_$SPECIES.out
-cat res_candidatsLRR_in_$SPECIES.out
+
 ## Constituer des hits globaux par proteines en 2 temps : 1er tour avec des seuils hauts pour fixer des exons d"ancrage"
 ## puis deuxieme tour avec les hsp plus faibles pour completer les zones aux extremites et definir d'autres zones types paralogues
 
+
+## 1st run : high threshold
+## filtering and Sorting
+gawk -v treshold1=$treshold1 'BEGIN{OFS="\t"}{if($10>=treshold1){print($0)}}' res_candidatsLRR_in_$SPECIES.out | sort -k1,2 -Vk7,7 > sort_65_candidatsLRR_in_$SPECIES.out
+## Si un alignement donne la totalite de la sequence -> extraction region
+## sinon, est ce que le hit suivant est proche?... oui -> cumul
+## si alignement cumule < 60% de la prot --> elim
+
+## Set intron size with max intron in Nip for the prot
+
+gawk 'BEGIN{OFS="\t"}{
+		if(NR==FNR){
+			if($3=="gene"){gsub("ID=","",$9);new=1;ID=$9}
+			else{
+				if($3=="CDS"){
+					if(new==1){stop=$5;new=0;MAX_INTRON[ID]=0}
+					else{
+						if($4-stop>MAX_INTRON[ID]){MAX_INTRON[ID]=($4-stop)};stop=$5}}}}
+		else{if($7<$8){strand="+"}else{strand="-"};
+		limIntron=5000;if(MAX_INTRON[$1]>limIntron){limIntron=MAX_INTRON[$1]+500};
+		if(FNR==1){Q=$1;T=$2;P1=$7;P2=$8;old5=$5;old6=$6;S=strand;line=$0;}
+		else{
+			split(line,tab,"\t");
+			if($1==Q && $2==T && strand==S && ((strand=="+" && $6>old6 && old6<$5+100) || (strand=="-" && $5<old5 && $6<old5+100)) && ($(7)-P2<limIntron || $(8)-P1<limIntron)){
+				P1=$7;P2=$8;
+				$4=$4+tab[4];
+				old5=$5;
+				old6=$6;
+				if($5>tab[5]){$5=tab[5]};
+				if($6<tab[6]){$6=tab[6]};
+				if(strand=="+"){
+					if($7>tab[7]){$7=tab[7]};
+					if($8<tab[8]){$8=tab[8]};}
+				else{
+					if($7<tab[7]){$7=tab[7]};
+					if($8>tab[8]){$8=tab[8]};}
+				$9=$9+tab[9];
+				$11=$11+tab[11];
+				$13=$13+tab[13]
+				$10=($9/$4)*100;
+				line=$0;}
+			else{
+				print(line);Q=$1;T=$2;P1=$7;P2=$8;old5=$5;old6=$6;S=strand;line=$0}}
+}}END{print(line)}' $GFF sort_65_candidatsLRR_in_$SPECIES.out | sort -k2,2 -Vk7,7 > concat_65_candidatsLRR_in_$SPECIES.tmp
+
+## 2nd run : lower threshold
+##------------------------------
+gawk -v treshold1=$treshold1 -v treshold2=$treshold2 'BEGIN{OFS="\t"}{if($10>treshold2 && $10<treshold1){print($0)}}' res_candidatsLRR_in_$SPECIES.out | sort -k1,2 -Vk7,7 > sort_45_candidatsLRR_in_$SPECIES.out
+
+##we discarded hits falling inside already identified regions
+cat concat_65_candidatsLRR_in_$SPECIES.tmp sort_45_candidatsLRR_in_$SPECIES.out | sort -k1,2 -Vk7,7 | gawk 'BEGIN{OFS="\t"}{if(NR==1){query=$1;target=$2;p7=$7;p8=$8;print}else{if($1!=query || $2!=target || ($7<$8 && $8>p8) || ($7>$8 && $7>p7)){print;p7=$7;p8=$8;query=$1;target=$2}}}' > concat_candidatsLRR_in_$SPECIES.tmp
+
+gawk 'BEGIN{OFS="\t"}{
+		if(NR==FNR){
+			if($3=="gene"){gsub("ID=","",$9);new=1;ID=$9}
+			else{
+				if($3=="CDS"){
+					if(new==1){stop=$5;new=0;MAX_INTRON[ID]=0}
+					else{
+						if($4-stop>MAX_INTRON[ID]){MAX_INTRON[ID]=($4-stop)};stop=$5}}}}
+		else{if($7<$8){strand="+"}else{strand="-"};
+		limIntron=5000;if(MAX_INTRON[$1]>limIntron){limIntron=MAX_INTRON[$1]+500};
+		if(FNR==1){Q=$1;T=$2;P1=$7;P2=$8;old5=$5;old6=$6;S=strand;line=$0;}
+		else{
+			split(line,tab,"\t");
+			if($1==Q && $2==T && strand==S && ((strand=="+" && $6>old6 && old6<$5+100) || (strand=="-" && $5<old5 && $6<old5+100)) && ($(7)-P2<limIntron || $(8)-P1<limIntron)){
+				P1=$7;P2=$8;
+				$4=$4+tab[4];
+				old5=$5;
+				old6=$6;
+				if($5>tab[5]){$5=tab[5]};
+				if($6<tab[6]){$6=tab[6]};
+				if(strand=="+"){
+					if($7>tab[7]){$7=tab[7]};
+					if($8<tab[8]){$8=tab[8]};}
+				else{
+					if($7<tab[7]){$7=tab[7]};
+					if($8>tab[8]){$8=tab[8]};}
+				$9=$9+tab[9];
+				$11=$11+tab[11];
+				$13=$13+tab[13]
+				$10=($9/$4)*100;
+				line=$0;}
+			else{
+				if((tab[6]-tab[5]+1)/tab[3]>=0.6 && tab[10]>=50){print(line)};Q=$1;T=$2;P1=$7;P2=$8;old5=$5;old6=$6;S=strand;line=$0}}
+}}END{print(line)}' $GFF concat_candidatsLRR_in_$SPECIES.tmp | sort -k2,2 -Vk7,7 > concat_candidatsLRR_in_$SPECIES.out
+
+## Regions candidates par query --> a filtrer pour enlever les redondances
+## Garder pour une zone d'interet, la query donnant la meilleure couverture 
+
+gawk -F"\t" 'BEGIN{OFS="\t"}{
+       if(NR==1){T=$2;P1=$7;P2=$8;score=$(13);line=$0}
+       else{
+          if($2==T && ($(7)<P2 || P1>$(8))){
+              if(score<$(13)){T=$2;P1=$7;P2=$8;score=$(13);line=$0} 
+          }else{
+              print(line);T=$2;P1=$7;P2=$8;line=$0;score=$(13)}}
+}END{print(line)}' concat_candidatsLRR_in_$SPECIES.out > filtered_candidatsLRR_in_$SPECIES.out
+
+gawk -F"\t" 'BEGIN{OFS="\t";}{if(NR==FNR){CHR[FNR]=$2;if($7<$8){START[FNR]=$7;STOP[FNR]=$8}else{START[FNR]=$8;STOP[FNR]=$7}}
+                            else{if($2!=CHR[FNR-1]){s1="0";current=$2}else{s1=STOP[FNR-1]};
+								 if($2!=CHR[FNR+1]){if($7<$8){s2=$8+5000}else{s2=$7+5000}}else{s2=START[FNR+1]};print($0,s1,s2)}}' filtered_candidatsLRR_in_$SPECIES.out filtered_candidatsLRR_in_$SPECIES.out > filtered_candidatsLRR_in_$SPECIES.out2
+
+# Extract regions of interest + 300bp before and after
+# gff format and query/target list
+gawk -v sp=$SPECIES 'BEGIN{OFS="\t";}{
+           if($7<$8){
+               if($5>10){$7=$7-3000}else{$7=$7-300};
+               if($7<=$(14)){$7=$(14)+1};
+               if($6<$3-10){$8=$8+3000}else{$8=$8+300};
+			   if($8>=$(15)){$8=$(15)-1};
+               pos=sprintf("%08d",$7);
+               print($2,"TransfertAnnot","gene",$7,$8,".","+",".","ID="sp"_"$2"_"pos";Origin="$1);print(sp"_"$2"_"pos,$1,"+")>"liste_query_target.txt"}
+           else{
+               if($6<$3-10){$8=$8-3000}else{$8=$8-300};
+			   if($8<=$(14)){$8=$(14)+1};
+               if($5>10){$7=$7+3000}else{$7=$7+300};
+               if($7>=$(15)){$7=$(15)-1};
+               pos=sprintf("%08d",$7);
+               print($2,"TransfertAnnot","gene",$8,$7,".","-",".","ID="sp"_"$2"_"pos";Origin="$1);print(sp"_"$2"_"pos,$1,"-")>"liste_query_target.txt"}
+}' filtered_candidatsLRR_in_$SPECIES.out2 > filtered_candidatsLRR_in_$SPECIES.gff
+
+cat liste_query_target.txt > candidate_loci_to_LRRome
+
+cat filtered_candidatsLRR_in_$SPECIES.gff > filtered_candidatsLRR
+echo cest ici
+cat filtered_candidatsLRR
+echo ----------------------
+
+
+python3 $SCRIPT/Extract_sequences_from_genome.py -f $BLASTDB -g filtered_candidatsLRR_in_$SPECIES.gff -o ./DNA_candidatsLRR_in_$SPECIES.fasta  -t gene 
+echo "python3 $SCRIPT/Extract_sequences_from_genome.py -f $BLASTDB -g filtered_candidatsLRR_in_$SPECIES.gff -o ./DNA_candidatsLRR_in_$SPECIES.fasta  -t gene "
+cat ./DNA_candidatsLRR_in_$SPECIES.fasta > xDNA_candidatsLRR_in 
+mkdir CANDIDATE_SEQ_DNA ; cd CANDIDATE_SEQ_DNA
+extractSeq ../DNA_candidatsLRR_in_$SPECIES.fasta
