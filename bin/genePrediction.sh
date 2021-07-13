089bff67858d164cdc28dc56a7d6e66ca0af5e98
Thibaud Vicat <57406915+Thiabud@users.noreply.github.com>
2021-07-13 19:53:41 +0200

    bestCorrection

--- bin/genePrediction.sh
+++ bin/genePrediction.sh
@@ -282,7 +282,7 @@ cat filtered7_LRRlocus_in_${SPECIES}_prot.gff > $resDir/prot2genomeForBest_one_c
 
 blastbest=((echo "$covblast"*0.6+(echo "$blastForBest"/100)*0.4 |bc -l))
 cdnabest=$($covcdna*1000)
-protbest=`echo "($covprot*100)*0.4+ ($protbest*0.6)" | bc`
+protbest=$(awk '{print ($1/100)*0.6 + $2*0.4}' <<<"${protbest} ${covprot}")
 if [ $mode == "first" ] 
 then
 	if [ -s mapping_LRRlocus_${SPECIES}.gff ]
