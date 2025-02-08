bbc02266da786c611af0f1cba80cc9677d917b4a
Johanna Girodolle <38213007+JohGi@users.noreply.github.com>
2025-02-08 18:06:23 +0100

    Integrate the new candidateLoci_VR.py script in the workflow

--- bin/candidateLoci.sh
+++ bin/candidateLoci.sh
@@ -5,9 +5,9 @@
 # AUTHOR : Celine Gottin & Thibaud Vicat & Vincent Ranwez
 # CREATION : 2020.02.20
 #========================================================
-# DESCRIPTION : Use of mmseqs to find regions of interest in the 
-#               target genome from protein sequences contained in LRRome 
-#               and create query/target pairs returns this LRRome 
+# DESCRIPTION : Use of mmseqs to find regions of interest in the
+#               target genome from protein sequences contained in LRRome
+#               and create query/target pairs returns this LRRome
 # ARGUMENTS : o $1 : Path to the Target genome fasta file
 #             o $2 : Path to the LRRome directory
 #             o $3 : Path to the reference GFF
@@ -41,7 +41,7 @@ mmseqs="mmseqs"
 #========================================================
 # $1 parameter allows to specify a prefix to identify your tmp folders
 function get_tmp_dir(){
-  local tmp_dir; 
+  local tmp_dir;
   tmp_dir=$(mktemp -d -t "$1"_$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXXXX)
   echo $tmp_dir
 }
@@ -64,61 +64,18 @@ function extractSeq {
 #========================================================
 
 
-          #------------------------------------------#
-          # 1. Find Regions of interest with mmseqs2 #
-          #------------------------------------------#
-
 tmpdir=$(get_tmp_dir LRRtransfer_candidateLoci)
 cd $tmpdir
 
-filename=$(basename ${TARGET_GENOME%.fasta})
-
-## using mmseqs
-#$mmseqs createdb $TARGET_GENOME ${filename}_db -v 0
-#$mmseqs createdb $LRRome/REF_proteins.fasta prot_db  -v 0 
-#$mmseqs search prot_db ${filename}_db resultDB_aln.m8 tmp -s 8.5 -a -e 0.1 --min-length 10 --merge-query 1 --cov-mode 2 --max-seqs 30000 --sequence-overlap 1000 -v 0
-#$mmseqs convertalis prot_db ${filename}_db resultDB_aln.m8 res_candidatsLRR.out --format-output query,target,qlen,alnlen,qstart,qend,tstart,tend,nident,pident,gapopen,evalue,bits  -v 0
-
-## using blast+
-#makeblastdb -in $TARGET_GENOME -out ${filename}_db -dbtype nucl
-#tblastn -db ${filename}_db -query $LRRome/REF_proteins.fasta -evalue 1 -out res_candidatsLRR.out -outfmt "6 qseqid sseqid qlen length qstart qend sstart send nident pident gapopen evalue bitscore"
-
-cp ${blast_res} res_candidatsLRR.out
+python ${LRR_SCRIPT}/candidate_loci_VR.py -g $REF_GFF -t ${blast_res} -o filtered_candidatsLRR.gff -l list_query_target.txt
 
 
-          #------------------------------------------#
-          # 2. Extract and concat hits               #
-          #------------------------------------------#
+python $LRR_SCRIPT/Extract_sequences_from_genome.py -f $TARGET_GENOME -g filtered_candidatsLRR.gff -o DNA_candidatsLRR.fasta  -t gene
 
-## Build up global hits by proteins in 2 steps: 
-## 1st round with high thresholds to fix "anchor" exons
-## 2nd round with lower ths to complete the areas at the ends and define other paralogous type areas
-sort -k1,2 -Vk7,7 res_candidatsLRR.out > tmp.tmp
-mv tmp.tmp res_candidatsLRR.out
-python ${LRR_SCRIPT}/filter_res_align.py -g $REF_GFF -t res_candidatsLRR.out > concat_candidatsLRR.out
-
-
-
-          #------------------------------------------#
-          # 3. format Candidate regions              #
-          #------------------------------------------#
-
-sort -k2,2 -Vk7,7 concat_candidatsLRR.out > tmp.tmp
-mv tmp.tmp concat_candidatsLRR.out
-python ${LRR_SCRIPT}/create_candidate_from_align.py -t concat_candidatsLRR.out -o filtered_candidatsLRR.gff > list_query_target.txt
-
-
-
-          #------------------------------------------#
-          # 5. Export files                          #
-          #------------------------------------------#
-
-python $LRR_SCRIPT/Extract_sequences_from_genome.py -f $TARGET_GENOME -g filtered_candidatsLRR.gff -o DNA_candidatsLRR.fasta  -t gene 
-
-mkdir CANDIDATE_SEQ_DNA 
+mkdir CANDIDATE_SEQ_DNA
 cd CANDIDATE_SEQ_DNA
 extractSeq ../DNA_candidatsLRR.fasta
-cd .. 
+cd ..
 
 #saving files
 cp list_query_target.txt $RES_DIR/.
